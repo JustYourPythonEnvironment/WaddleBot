6fbf84de793992da85c44647785f572bebc7a8c4
Jason Manson-Hing <jason.mansonhing@gmail.com>
2018-10-04 19:41:12 -0400

    Hotfix for incorrect error handling

--- commands/search.js
+++ commands/search.js
@@ -25,16 +25,16 @@ module.exports = {
                 const snapshot = await client.database.ref(`reactions/`).orderByKey().once('value');
                 const mediaData = snapshot.val();
                 const sortedMediaNames = collection.groupBy(Object.keys(mediaData).filter(key => key.includes(searchKey)), results => 'results');
-                const results = sortedMediaNames.results;
+                const results = sortedMediaNames.results || [];
                 const numResults = results.length;
                 if (numResults > 0) {
                     message.channel.send(`I found ${numResults} reaction${numResults > 1 ? 's' : ''} that contained ${searchKey}: ${formatArrayAsList(results)}.`);
                 } else {
-                    message.channel.send(`TT! I coudln't find any reactions containing ${searchTerm}!`);
+                    message.channel.send(`TT! I coudln't find any reactions containing \`${searchKey}\`!`);
                 }
             } catch (err) {
                 console.error(err);
-                message.channel.send(`TT! I coudln't find any reactions containing ${searchTerm} because: ${err}`);
+                message.channel.send(`TT! I coudln't find any reactions containing \`${searchKey}\` because: ${err}`);
             }
         }
         return;
