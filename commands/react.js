5a73002d115d373a590dacdd5909acc30a389459
leungbyr <byron.leung@mail.utoronto.ca>
2020-02-19 02:54:54 -0500

    Add finding similar reaction if not found

--- commands/react.js
+++ commands/react.js
@@ -1,6 +1,7 @@
 const helpEmbed = require('../embeds/helpEmbed.js');
 const Utils = require('../utils/Utils.js');
 const { HELP, HELP_SHORT } = require('../assets/flags.json');
+const stringSimilarity = require('string-similarity');
 
 const configuration = {
   enabled: true,
@@ -24,8 +25,19 @@ module.exports = {
         const mediaSnapshot = await client.database.ref(`reactions/${aliasSnapshot.val()}/media`).once('value');
         message.channel.send(mediaSnapshot.val());
       } catch (err) {
-        console.error(err);
-        message.channel.send(`TT! I can't find ${args[0]}!`);
+        // find most similar reaction if not found
+        const snapshot = await client.database.ref(`reactions/`).once('value');
+        const mediaKeys = Object.keys(snapshot.val());
+        const bestMatch = stringSimilarity.findBestMatch(args[0], mediaKeys).bestMatch.target;
+
+        try {
+          const aliasSnapshot = await client.database.ref(`aliases/${bestMatch}`).once('value');
+          const mediaSnapshot = await client.database.ref(`reactions/${aliasSnapshot.val()}/media`).once('value');
+          message.channel.send(mediaSnapshot.val());
+        } catch (err) {
+          console.error(err);
+          message.channel.send(`TT! I can't find ${args[0]}!`);
+        }
       }
     }
     return;
