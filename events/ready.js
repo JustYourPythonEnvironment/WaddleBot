3a7d3ec78e95a0b87966fc4fc84357af22f6856b
leungbyr <byron.leung@mail.utoronto.ca>
2020-05-02 18:49:09 -0400

    removing all handlers for reactions db update event before attaching new

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,9 @@ const { updateReactionsList } = require('../utils/Utils.js');
 module.exports = async (client) => {
   console.log('Ready!');
   client.user.setActivity(`with your heart.`);
-  client.database.ref('reactions/').orderByKey().on('value', snapshot => {
+  const reactionsQuery = client.database.ref('reactions/').orderByKey();
+  reactionsQuery.off(); // clears all event handlers
+  reactionsQuery.on('value', snapshot => {
     const reactionsChannel = client.channels.find(ch => ch.name === 'reaction-database');
     updateReactionsList(snapshot, reactionsChannel);
   });
